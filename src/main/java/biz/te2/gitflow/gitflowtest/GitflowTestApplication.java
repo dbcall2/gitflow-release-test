9aec0d6503301e4c3e65afbdca18b8aac98cc6df
briancall <briancall@172.17.0.70>
2018-10-11 09:28:27 -0400

    Initial project create.

---
+++ src/main/java/biz/te2/gitflow/gitflowtest/GitflowTestApplication.java
@@ -0,0 +1,12 @@
+package biz.te2.gitflow.gitflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowTestApplication.class, args);
+	}
+}
